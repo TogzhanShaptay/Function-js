1bac5bdf894208f391de79ff3aeb3d0157bf47c6
TogzhanShaptay <96471645+TogzhanShaptay@users.noreply.github.com>
2021-12-21 17:46:04 +0600

    js files

---
+++ function-javascipt.js
@@ -0,0 +1,61 @@
+function getMilk(money) {   
+
+  console.log("leaveHouse");
+
+  console.log("moveRight");
+
+  console.log("moveRight");
+
+  console.log("moveUp");
+
+  console.log("moveUp");
+
+  console.log("moveUp");
+  var bottlesOfMilk = Math.floor(money/ 5);
+
+  console.log("buy " + bottlesOfMilk + " bottles of milk");
+
+  console.log("moveRight");
+
+  console.log("moveRight");
+
+  console.log("moveLeft");
+
+  console.log("moveLeft");
+
+  console.log("moveDown");
+
+  console.log("moveDown");
+
+  console.log("moveDown");
+
+  console.log("moveDown");
+
+  console.log("moveLeft");
+
+  console.log("moveLeft");
+
+  console.log("enterHouse");
+
+}
+ getMilk (12); // 12/2 = 6
+
+
+
+
+
+
+
+
+    
+
+    var months = (90*12) - (age*12);
+    var weeks = (90*52) - (age*52);
+    var days = (90*365) - (age*365);
+  
+   console.log ("Right now you are " + age + " years old" );
+   console.log ("You have " + months + " months, " + weeks + " weeks, " + days +  " days left");
+ 
+
+    return weeks
+}
